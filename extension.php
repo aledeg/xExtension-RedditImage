a1ffa0a7df76bdd91b35ddd71c7bf2ab930eb577
Alexis Degrugillier <github@ainw.org>
2017-09-30 13:37:35 +0200

    Add an extension to replace Reddit links with resource links
    If the resource linked is an image, it is added to the content.

---
+++ extension.php
@@ -0,0 +1,30 @@
+<?php
+
+class RedditImageExtension extends Minz_Extension {
+	public function init() {
+		$this->registerHook('entry_before_display', array($this, 'transformLink'));
+	}
+	
+	public function transformLink($entry) {
+		$link = $entry->link();
+		
+		if (0 === strpos('reddit.com', $link)) {
+			return $entry;
+		}
+		
+		$content = $entry->content();
+
+		// Change entry link by content link
+		if (preg_match('/<a href="([^"]*)">\[link\]<\/a>/', $content, $matches)) {
+			$href = $matches[1];
+                        $entry->_link($href);
+		}
+
+		// Add image tag in content when the href links to an image
+		if (preg_match('/(jpg|png|gif|bmp)$/', $href)) {
+			$entry->_content(sprintf('%s <img src="%s" />', $content, $href));
+		}
+		
+		return $entry;
+	}
+}
