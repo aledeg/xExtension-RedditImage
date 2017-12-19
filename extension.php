ddae75dc5959540e899f76340d9b9644bfd5f64f
Alexis Degrugillier <github@ainw.org>
2017-12-19 07:08:59 +0100

    Add support for Imgur images

--- extension.php
+++ extension.php
@@ -21,8 +21,11 @@ class RedditImageExtension extends Minz_Extension {
 		}
 
 		// Add image tag in content when the href links to an image
-		if (preg_match('/(jpg|png|gif|bmp)$/', $href)) {
-			$entry->_content(sprintf('%s <img src="%s" class="reddit-image" />', $content, $href));
+		if (preg_match('#(jpg|png|gif|bmp)(\?.*)?$#', $href)) {
+			$entry->_content(sprintf('%s <img src="%s" class="reddit-image" alt="URL" />', $content, $href));
+		// Add image tag in content when the href links to an imgur image
+		} elseif (preg_match('#(?P<imgur>imgur.com/[^/]*)$#', $href)) {
+			$entry->_content(sprintf('%s <img src="%s.png" class="reddit-image alt="IMGUR" />', $content, $href));
 		} else {
 			$entry->_content(sprintf('%s <p>%s</p>', $content, $href));
 		}
