90aa57a9fe1f05c6ae2560b1ced888589232c12e
Alexis Degrugillier <github@ainw.org>
2017-12-22 23:54:43 +0100

    Change display of the modified content
    Before, the image/video was added after the original content. As the original content
    rarely got something interesting when it links to images/videos, the image/video is
    added before the original content.

--- extension.php
+++ extension.php
@@ -22,19 +22,19 @@ class RedditImageExtension extends Minz_Extension {
 
 		// Add image tag in content when the href links to an image
 		if (preg_match('#(jpg|png|gif|bmp)(\?.*)?$#', $href)) {
-			$entry->_content(sprintf('%s <img src="%s" class="reddit-image" alt="IMAGE URL" />', $content, $href));
+			$entry->_content(sprintf('<img src="%2$s" class="reddit-image" alt="IMAGE URL" /> %1$s', $content, $href));
 		// Add image tag in content when the href links to an imgur gifv
 		} elseif (preg_match('#(?P<gifv>.*imgur.com/[^/]*).gifv$#', $href, $matches)) {
-			$entry->_content(sprintf('%s <img src="%s.gif" class="reddit-image" alt="IMGUR GIFV" />', $content, $matches['gifv']));
+			$entry->_content(sprintf('<img src="%2$s.gif" class="reddit-image" alt="IMGUR GIFV" /> %1$s', $content, $matches['gifv']));
 		// Add image tag in content when the href links to an imgur image
 		} elseif (preg_match('#(?P<imgur>imgur.com/[^/]*)$#', $href)) {
-			$entry->_content(sprintf('%s <img src="%s.png" class="reddit-image" alt="IMGUR TOKEN" />', $content, $href));
+			$entry->_content(sprintf('<img src="%2$s.png" class="reddit-image" alt="IMGUR TOKEN" /> %1$s', $content, $href));
 		// Add video tag in content when the href links to a video
 		} elseif (preg_match('#(?P<extension>webm|mp4)$#', $href, $matches)) {
-			$entry->_content(sprintf('%s  <video controls><source src="%s" type="video/%s">VIDEO URL</video>', $content, $href, $matches['extension']));
+			$entry->_content(sprintf('<video controls><source src="%2$s" type="video/%3$s">VIDEO URL</video> %1$s', $content, $href, $matches['extension']));
 		// Add video tag in content when the href links to a gfycat video
 		} elseif (preg_match('#(?P<gfycat>gfycat.com/)(.*/)*(?P<token>[^/.]*)$#', $href, $matches)) {
-			$entry->_content(sprintf('%s <video controls><source src="https://giant.%s%s.mp4" type="video/mp4">GFYCAT TOKEN</video>', $content, $matches['gfycat'], $matches['token']));
+			$entry->_content(sprintf('<video controls><source src="https://giant.%2$s%3$s.mp4" type="video/mp4">GFYCAT TOKEN</video> %1$s', $content, $matches['gfycat'], $matches['token']));
 		} else {
 			$entry->_content(sprintf('%s <p>%s</p>', $content, $href));
 		}
