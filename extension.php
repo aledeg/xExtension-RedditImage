adda46a87cc27c2274868ccf2f9bf6909ab908a7
Alexis Degrugillier <github@ainw.org>
2019-03-16 18:18:38 +0100

    Change content handling
    The link is modified only if there is no content recognized.
    If there is recognized content, the entry content is replaced by
    the linked resource content.
    Before inserting in the database, the gfycat links are modified
    to the video URL by querying the API.

--- extension.php
+++ extension.php
@@ -1,44 +1,89 @@
 <?php
 
 class RedditImageExtension extends Minz_Extension {
+    const IMAGE_CONTENT = '<a href="%1$s"><img src="%1$s" class="reddit-image"/></a>';
+    const VIDEO_CONTENT = '<video controls class="reddit-image"><source src="%1$s" type="video/%2$s"></video>';
+    const GFYCAT_API = 'https://api.gfycat.com/v1/gfycats/%s';
+    const MATCH_REDDIT = 'reddit.com';
+
 	public function init() {
-		$this->registerHook('entry_before_display', array($this, 'transformLink'));
+		$this->registerHook('entry_before_display', array($this, 'transformEntry'));
+		$this->registerHook('entry_before_insert', array($this, 'updateGfycatLink'));
 	}
 
-	public function transformLink($entry) {
-		$link = $entry->link();
+	public function transformEntry($entry) {
+		if (false === $this->isRedditLink($entry)) {
+		    return $entry;
+        }
 
-		if (false === strpos($link, 'reddit.com')) {
-			return $entry;
-		}
-
-		$content = $entry->content();
-
-		// Change entry link by content link
-		if (preg_match('#<a href="(?P<href>[^"]*)">\[link\]</a>#', $content, $matches)) {
-			$href = $matches['href'];
-			$entry->_link($href);
-		}
+		if (null === $href = $this->getContentLink($entry)) {
+		    return $entry;
+        }
 
 		// Add image tag in content when the href links to an image
 		if (preg_match('#(jpg|png|gif|bmp)(\?.*)?$#', $href)) {
-			$entry->_content(sprintf('<img src="%2$s" class="reddit-image" alt="IMAGE URL" /> %1$s', $content, $href));
+            $this->addImageContent($entry, $href);
 		// Add image tag in content when the href links to an imgur gifv
 		} elseif (preg_match('#(?P<gifv>.*imgur.com/[^/]*).gifv$#', $href, $matches)) {
-			$entry->_content(sprintf('<img src="%2$s.gif" class="reddit-image" alt="IMGUR GIFV" /> %1$s', $content, $matches['gifv']));
+		    $this->addImageContent($entry, "${matches['gifv']}.gif");
 		// Add image tag in content when the href links to an imgur image
 		} elseif (preg_match('#(?P<imgur>imgur.com/[^/]*)$#', $href)) {
-			$entry->_content(sprintf('<img src="%2$s.png" class="reddit-image" alt="IMGUR TOKEN" /> %1$s', $content, $href));
+		    $this->addImageContent($entry, "${href}.png");
 		// Add video tag in content when the href links to a video
 		} elseif (preg_match('#(?P<extension>webm|mp4)$#', $href, $matches)) {
-			$entry->_content(sprintf('<video controls class="reddit-image"><source src="%2$s" type="video/%3$s">VIDEO URL</video> %1$s', $content, $href, $matches['extension']));
-		// Add video tag in content when the href links to a gfycat video
-		} elseif (preg_match('#(?P<gfycat>gfycat.com/)(.*/)*(?P<token>[^/.]*)$#', $href, $matches)) {
-			$entry->_content(sprintf('<video controls class="reddit-image"><source src="https://giant.%2$s%3$s.mp4" type="video/mp4">GFYCAT TOKEN</video> %1$s', $content, $matches['gfycat'], $matches['token']));
+		    $this->addVideoContent($entry, $href, $matches['extension']);
 		} else {
-			$entry->_content(sprintf('%s <p>%s</p>', $content, $href));
-		}
+		    $entry->_link($href);
+        }
 
 		return $entry;
 	}
+
+	public function updateGfycatLink($entry) {
+        if (false === $this->isRedditLink($entry)) {
+            return $entry;
+        }
+
+        if (null === $href = $this->getContentLink($entry)) {
+            return $entry;
+        }
+
+        if (preg_match('#(?P<gfycat>gfycat.com/)(.*/)*(?P<token>[^/.]*)$#', $href, $matches)) {
+            try {
+                $jsonResponse = file_get_contents(sprintf(static::GFYCAT_API, $matches['token']));
+                $arrayResponse = json_decode($jsonResponse, true);
+                $videoUrl = $arrayResponse['gfyItem']['mp4Url'];
+                $newContent = preg_replace('#<a href="(?P<href>[^"]*)">\[link\]</a>#', "<a href=\"${videoUrl}\">[link]</a>", $entry->content());
+            } catch (Exception $e) {
+                $newContent = sprintf('%s <p>GFYCAT ERROR</p>', $entry->content());
+            }
+            $entry->_content($newContent);
+        }
+
+        return $entry;
+    }
+
+    /**
+     * @return bool
+     */
+    private function isRedditLink($entry) {
+	    return (bool) strpos($entry->link(), static::MATCH_REDDIT);
+    }
+
+    /**
+     * @return string|null
+     */
+    private function getContentLink($entry) {
+	    if (preg_match('#<a href="(?P<href>[^"]*)">\[link\]</a>#', $entry->content(), $matches)) {
+		    return $matches['href'];
+        }
+    }
+
+	private function addImageContent($entry, $href) {
+	    $entry->_content(sprintf(static::IMAGE_CONTENT,$href));
+    }
+
+    private function addVideoContent($entry, $href, $extension) {
+	    $entry->_content(sprintf(static::VIDEO_CONTENT, $href, $extension));
+    }
 }
