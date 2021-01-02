431d18d660ff73db93b1ae5b4525560725cc26c9
rom-1 <37835829+rom-1@users.noreply.github.com>
2021-01-03 00:37:41 +0100

    Created German Language File

---
+++ i18n/de/ext.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    'reddit_image' => array(
+        'display_image' => 'Bilder anzeigen',
+        'display_original' => 'Originalinhalt anzeigen',
+        'display_video' => 'Videos anzeigen',
+        'image_height' => 'Max. Höhe der Mediendatei',
+        'image_height_unit' => '%% des Darstellungsbereichs (Viewport)',
+        'muted_video' => 'Videos stummschalten',
+        'permission_problem' => 'Konnte die Konfigurationsdatei nicht überschreiben, bitte Dateiberechtigungen anpassen für %s',
+    ),
+);
