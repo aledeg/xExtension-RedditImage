fa957271de4170d8036a8901c4b3f1178447679d
Alexis Degrugillier <github@ainw.org>
2021-01-03 22:48:38 -0500

    Remove unnecessary code
    Before, there was a parameter in the video transformation that was
    called but not used.
    Now, the useless parameter has been removed.
    
    Before, the configuration was retrieved for each article.
    Now, it is retrieved only once during the init process.

--- extension.php
+++ extension.php
@@ -2,7 +2,7 @@
 
 class RedditImageExtension extends Minz_Extension {
     const IMAGE_CONTENT = '<div class="reddit-image figure"><img src="%s" class="reddit-image"/></div>';
-    const VIDEO_CONTENT = '<div class="reddit-image figure"><video controls preload="metadata" %4$s class="reddit-image"><source src="%1$s" type="video/webm"><source src="%2$s" type="video/mp4"></video></div>';
+    const VIDEO_CONTENT = '<div class="reddit-image figure"><video controls preload="metadata" %3$s class="reddit-image"><source src="%1$s" type="video/webm"><source src="%2$s" type="video/mp4"></video></div>';
     const LINK_CONTENT = '<p><a href="%1$s">%1$s</a></p>';
     const GFYCAT_API = 'https://api.gfycat.com/v1/gfycats/%s';
     const REDGIFS_API = 'https://api.redgifs.com/v1/gfycats/%s';
@@ -24,6 +24,8 @@ class RedditImageExtension extends Minz_Extension {
         if (file_exists($filepath)) {
             Minz_View::appendStyle($this->getFileUrl($filename, 'css'));
         }
+
+        $this->getConfiguration();
 
         $this->registerHook('entry_before_display', array($this, 'transformEntry'));
         $this->registerHook('entry_before_insert', array($this, 'updateGfycatLink'));
@@ -64,8 +66,6 @@ class RedditImageExtension extends Minz_Extension {
             return $entry;
         }
 
-        $this->getConfiguration();
-
         $content = '';
 
         // Add image tag in content when the href links to an image
@@ -81,7 +81,7 @@ class RedditImageExtension extends Minz_Extension {
             $content = $this->getNewImageContent($href);
         // Add video tag in content when the href links to a video
         } elseif (preg_match('#(?P<baseurl>.+)(webm|mp4)$#', $href, $matches)) {
-            $content = $this->getNewVideoContent($entry, $matches['baseurl']);
+            $content = $this->getNewVideoContent($matches['baseurl']);
         } else {
             $content = $this->getNewLinkContent($href);
         }
@@ -155,13 +155,13 @@ class RedditImageExtension extends Minz_Extension {
         return sprintf(static::IMAGE_CONTENT,$href);
     }
 
-    private function getNewVideoContent($entry, $baseUrl) {
+    private function getNewVideoContent($baseUrl) {
         if (!$this->display_video) {
             return;
         }
         $hrefMp4 = $baseUrl . 'mp4';
         $hrefWebm = $baseUrl . 'webm';
-        return sprintf(static::VIDEO_CONTENT, $hrefWebm, $hrefMp4, $entry->link(), $this->muted_video ? 'muted': '');
+        return sprintf(static::VIDEO_CONTENT, $hrefWebm, $hrefMp4, $this->muted_video ? 'muted': '');
     }
 
     private function getNewLinkContent($href) {
