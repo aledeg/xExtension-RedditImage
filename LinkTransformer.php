d86ad6383b66346fcd4a2ccd6542c51063300a9b
Alexis Degrugillier <github@ainw.org>
2021-01-09 09:08:55 -0500

    Add support for reddit galleries
    The support is limited because it's not really robust.
    It might break unexpectedly. We'll see how it goes.

--- LinkTransformer.php
+++ LinkTransformer.php
@@ -45,6 +45,24 @@ class LinkTransformer extends AbstractTransformer {
             } catch (Exception $e) {
                 Minz_Log::error("REDDIT API ERROR - {$href}");
             }
+        } elseif (preg_match('#reddit.com/gallery#', $href)) {
+            try {
+                $jsonResponse = file_get_contents("{$this->extractOriginalCommentsLink($entry)}.json");
+                $arrayResponse = json_decode($jsonResponse, true);
+                $pictures = $arrayResponse[0]['data']['children'][0]['data']['media_metadata'];
+                if (!empty($pictures)) {
+                    $content = '<div class="reddit-image figure">';
+                    foreach ($pictures as $id => $metadata) {
+                        list(,$extension) = explode('/', $metadata['m']);
+                        $content .= "<img src=\"https://i.redd.it/{$id}.{$extension}\" class=\"reddit-image\"/>";
+                    }
+                    $content .= '</div>';
+                    $entry->_content($content . $entry->content());
+                }
+                // $entry->_content('gallery');
+            } catch (Exception $e) {
+                Minz_Log::error("REDDIT API ERROR - {$href}");
+            }
         }
 
         return $entry;
